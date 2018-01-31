9ab9674aa601e4cb99ed9b89a51755702c83936e
Jay Pipes <jaypipes@gmail.com>
2018-01-31 13:54:06 -0500

    default token -> string output for symbols

--- src/parser/token.cc
+++ src/parser/token.cc
@@ -13,10 +13,6 @@ std::ostream& operator<< (std::ostream& out, const token_t& token) {
         out << "keyword[" << symbol_map::to_string(token.symbol) << "]";
         return out;
     }
-    if (token.is_punctuator()) {
-        out << symbol_map::to_string(token.symbol);
-        return out;
-    }
     if (token.is_literal()){
         // TODO(jaypipes): Add typing of literal...
         size_t len = (token.end - token.start);
@@ -31,6 +27,9 @@ std::ostream& operator<< (std::ostream& out, const token_t& token) {
     if (token.symbol == SYMBOL_COMMENT) {
         size_t len = (token.end - token.start);
         out << "comment[length: " << len << "]";
+        return out;
+    } else {
+        out << symbol_map::to_string(token.symbol);
         return out;
     }
     return out;
