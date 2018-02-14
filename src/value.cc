436401da78971fef1dbcc2e29f8d3519ca2a1c16
Jay Pipes <jaypipes@gmail.com>
2018-02-14 17:32:17 -0500

    break out value expression printing

--- src/value.cc
+++ src/value.cc
@@ -8,8 +8,33 @@
 
 namespace sqltoast {
 
+std::ostream& operator<< (std::ostream& out, const value_expression_t& ve) {
+    switch (ve.type) {
+        case VALUE_EXPRESSION_TYPE_LITERAL:
+            out << "literal[" << std::string(ve.lexeme.start, ve.lexeme.end) << ']';
+            break;
+        case VALUE_EXPRESSION_TYPE_COLUMN:
+            out << "column-reference[" << std::string(ve.lexeme.start, ve.lexeme.end) << ']';
+            break;
+        default:
+            out << "value-expression[" << std::string(ve.lexeme.start, ve.lexeme.end) << ']';
+            break;
+    }
+    return out;
+}
+
 std::ostream& operator<< (std::ostream& out, const row_value_constructor_t& rvc) {
-    out << std::string(rvc.lexeme.start, rvc.lexeme.end);
+    switch (rvc.rvc_type) {
+        case RVC_TYPE_VALUE_EXPRESSION:
+            {
+                const value_expression_t& ve = static_cast<const value_expression_t&>(rvc);
+                out << ve;
+            }
+            break;
+        default:
+            out << "row-value-constructor[" << std::string(rvc.lexeme.start, rvc.lexeme.end) << ']';
+            break;
+    }
     return out;
 }
 
