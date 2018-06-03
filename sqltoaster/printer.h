77b73f32db4b27ce7f1a317b407bef5f5265fec9
Jay Pipes <jaypipes@gmail.com>
2018-06-03 12:22:01 -0400

    Remove list item iomanip

--- sqltoaster/printer.h
+++ sqltoaster/printer.h
@@ -8,7 +8,6 @@
 #define SQLTOASTER_PRINTER_H
 
 #include <iostream>
-#include <iomanip>
 
 #include <sqltoast/sqltoast.h>
 
@@ -19,27 +18,20 @@ typedef enum output_format {
     OUTPUT_FORMAT_YAML
 } output_format_t;
 
-const long LIST_ITEM_OFF = 0;
-const long LIST_ITEM_ON = 1;
-// Informs the printer whether the current item to be printed is a list item
-const int LIST_ITEM_XALLOC_INDEX = 0;
-
 typedef struct printer {
     int iomanip_indexes[1];
     sqltoast::parse_result_t& res;
     output_format_t output_format;
     size_t indent_level;
+    bool list_item;
     printer(
             sqltoast::parse_result_t& res,
             std::ostream& out) :
         res(res),
         output_format(OUTPUT_FORMAT_DEFAULT),
-        indent_level(0)
-    {
-        iomanip_indexes[LIST_ITEM_XALLOC_INDEX] = std::ios_base::xalloc();
-        int idx_list_item = iomanip_indexes[LIST_ITEM_XALLOC_INDEX];
-        out.iword(idx_list_item) = LIST_ITEM_OFF;
-    }
+        indent_level(0),
+        list_item(false)
+    {}
     inline void indent_push(std::ostream& out) {
         indent_level++;
     }
@@ -53,15 +45,13 @@ typedef struct printer {
         return out;
     }
     inline void start_list(std::ostream& out) {
-        int idx_list_item = iomanip_indexes[LIST_ITEM_XALLOC_INDEX];
-        out.iword(idx_list_item) = LIST_ITEM_ON;
+        list_item = true;
     }
     inline void end_list(std::ostream& out) {
-        int idx_list_item = iomanip_indexes[LIST_ITEM_XALLOC_INDEX];
-        out.iword(idx_list_item) = LIST_ITEM_OFF;
+        list_item = false;
     }
     inline bool in_list(std::ostream& out) const {
-        return (out.iword(iomanip_indexes[LIST_ITEM_XALLOC_INDEX]) == LIST_ITEM_ON);
+        return list_item;
     }
 } printer_t;
 
